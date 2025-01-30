c52f7fcb0d1a5f3a29c007f5a6f0cf4d39a7a5b0
zaidalramadhani <zaidalramadhani@localhost>
2025-01-29 21:12:39 -0500

    Added new varaibles and method

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -2,6 +2,20 @@ package demo1;
 
 public class DemoTest3 {
 	public static void main(String[] args) {
-		System.out.println("Java");
+	System.out.println("ZAID & SALLY LIVE IN TAMPA");
+		
+		int Sally = 31; 
+		int Zaid = 36; 
+		
+		int result = Zaid + Sally; 
+		
+		DemoTest3 b = new DemoTest3 (); 
+		b.add("Zaid", 36);
+		
+		
+	}
+	
+	public void add (String name, int a) { 
+		System.out.println(name + a);
 	}
 }
