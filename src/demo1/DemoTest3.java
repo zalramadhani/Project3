337dc2c6281fbe9cadc039a17fec38951d913670
zalramadhani <zalramadhanii@gmail.com>
2025-01-29 21:29:34 -0500

    Update DemoTest3.java

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -11,6 +11,17 @@ public class DemoTest3 {
 
 		System.out.println(result);
 
+			int x = 10; 
+		int y = 20; 
+		if (x > y) { 
+			System.out.println("Greater"); 
+		}else{ 
+			System.out.println("Less"); 
+		}
+
+		//This is coming from GitHub
+		// Practicing Pulling
+
 	}
 
 	public void add(int a, int b) {
